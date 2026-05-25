d4fc3a9dabcd73807b789387b43aef8510f7a6ee
FARTEZ HUB <fartez127@gmail.com>
2026-05-25 20:03:27 +0700

    Delete FartezHub.lua

--- FartezHub.lua
+++
@@ -1,7 +0,0 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/fartez127-design/Fartezhubx2/refs/heads/main/GameList.lua"))()
-
-local URL = Games[game.PlaceId]
-
-if URL then
-    loadstring(game:HttpGet(URL))()
-end
