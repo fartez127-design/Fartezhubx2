e2b323b1330895aba11ce8e4415c8d22d7194556
FARTEZ HUB <fartez127@gmail.com>
2026-05-26 17:10:22 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,17 +1,17 @@
 local Games = {
-  [9452898647]  = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/Farte01", -- Farte01
-  [9491545867]  = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZ%20HUB%20X%20Break%20a%20Brainrot%20Egg", -- Break a Brainrot Egg
-  [9787206684]  = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXBeaLuckyBlock", -- Be a Lucky Block
-  [9792947201]  = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXSlimeRNG", -- Slime RNG
-  [9875383684]  = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUB-Be%20a%20Brainrot%20v1", -- Be a Brainrot v1
-  [9913934374]  = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBShovelSnowforBrainrots", -- Shovel Snow for Brainrots
-  [9925813578]  = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZ%20HUB%20X%20Throw%20Lucky%20Blocks%20for%20Brainrots!", -- Throw Lucky Blocks for Brainrots
-  [9928450968]  = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXBecomeInvisibleForBrainrots", -- Become Invisible for Brainrots
-  [9969095451]  = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXSurfforLuckyBlocks", -- Surf for Lucky Blocks
-  [9988290343]  = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXShootLASERSForBrainrots!", -- Shoot LASERS for Brainrots
-  [10004244222] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXKickaLuckyBlock", -- Kick a Lucky Block
-  [10004760418] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXRollerforBrainrots", -- Roller for Brainrots
-  [10045085816] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXBuildBridgetoBrainrots", -- Build Bridge to Brainrots
+    [9452898647] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/Farte01",
+    [9491545867] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZ%20HUB%20X%20Break%20a%20Brainrot%20Egg",
+    [9787206684] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXBeaLuckyBlock",
+    [9792947201] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXSlimeRNG",
+    [9875383684] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUB-Be%20a%20Brainrot%20v1",
+    [9913934374] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBShovelSnowforBrainrots",
+    [9925813578] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZ%20HUB%20X%20Throw%20Lucky%20Blocks%20for%20Brainrots!",
+    [9928450968] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXBecomeInvisibleForBrainrots",
+    [9969095451] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXSurfforLuckyBlocks",
+    [9988290343] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXShootLASERSForBrainrots!",
+    [10004244222] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXKickaLuckyBlock",
+    [10004760418] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXRollerforBrainrots",
+    [10045085816] = "https://raw.githubusercontent.com/fartez127-design/FARTEZHUB/refs/heads/main/FARTEZHUBXBuildBridgetoBrainrots",
 }
 
 return Games
